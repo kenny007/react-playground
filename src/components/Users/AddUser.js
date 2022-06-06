5a4dffc3620847ffa7bce00ccead04c3b70ad035
kenny007 <press.spark@gmail.com>
2022-06-07 02:22:12 +0300

    Add Wrapper Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from '../UI/ErrorModal';
-
+import Wrapper from '../Helpers/Wrapper';
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
@@ -42,8 +42,10 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
-             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+        <Wrapper>
+            {error &&
+                <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />
+            }
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
@@ -63,7 +65,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     )
 };
 
