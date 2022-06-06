0673ad25eaeb30bfd379aa0d0918409261f329ad
kenny007 <press.spark@gmail.com>
2022-06-07 01:02:34 +0300

    Adds error modal message

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
+     {usersList.length > 0 && <UsersList users={usersList} />} 
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,15 +9,21 @@ import classes from './AddUser.module.css'
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-
+    const [error, setError] = useState();
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            })
             return;
         }
         if (+enteredAge < 1) { return; }
-
-        // console.log(enteredUsername, enteredAge);
+        setError({
+            title: 'Invalid age',
+            message: 'Please enter a valid age (> 0).'
+        })
         props.onAddUser(enteredUsername, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
@@ -33,7 +39,7 @@ const AddUser = (props) => {
 
     return (
         <div>
-            <ErrorModal title="An error occured!" message="Something went wrong!" />
+             {error && <ErrorModal title={error.title} message={error.message} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
